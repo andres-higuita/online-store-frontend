4c7519305e56c55130cc82c64812a021734d97d5
Andres Higuita <andreshiguita@MacBook-Air-de-Andres.local>
2025-05-23 16:02:07 -0500

    frist commit front

---
+++ next.config.js
@@ -0,0 +1,56 @@
+const checkEnvVariables = require("./check-env-variables")
+
+checkEnvVariables()
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: "/_next/static/chunks/app/:folder*/@login/:path*",
+          destination: "/_next/static/chunks/app/:folder*/%40login/:path*",
+        },
+        // Repeat this pattern if other similar errors occur
+      ],
+      afterFiles: [],
+      fallback: [],
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-server-testing.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
